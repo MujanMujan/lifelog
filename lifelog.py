31b85841461c1cf06c4e3aed246c958441c8e04c
Mujan <m.abbasi.72@gmail.com>
2016-12-12 21:52:25 +0330

    Add files via upload
    version 0.01

---
+++ lifelog.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3.5
+
+## import required module
+
+import time;
+import sqlite3;
+import os;
+import sys;
+
+## get localtime of system
+localtime = time.asctime( time.localtime(time.time()) )
+
+## just print welcome message
+print("welcome to lifelog application\nthe current time is :", localtime, "\n\n\n")
+
+
+## database connection and table creation
+conn = sqlite3.connect('lldb.db')
+conn.execute('''CREATE TABLE IF NOT EXISTS LOGS
+       (ID INTEGER PRIMARY KEY AUTOINCREMENT    NOT NULL,
+       DATE           TEXT    NOT NULL,
+       DETAIL            TEXT     NOT NULL);''')
+
+## main of program :
+try:
+	if sys.argv[1] == "-m":
+		log = sys.argv[2]
+		conn.execute('insert into LOGS(DATE,DETAIL)values ("%s","%s")'%(localtime,log))
+		conn.commit()
+	elif sys.argv[1] == "-h":
+		number_of_record = sys.argv[2]
+		cu = conn.execute('select * from (select * from LOGS order by ID DESC limit ' + number_of_record + ') order by ID ASC;')
+		for row in cu:
+			print(str(row[0]) +  "| " + row[1] + "| log: "+ row[2])
+	else:
+		print("else")
+except:
+	print("\nhelp!")
+	print("to add a log use '-m' flag\nexmp: lifelog -m 'message want to save'")
+	print("to see history of logs use '-h' flag\nexmp: lifelog -h <number of lastest record>")
+	sys.exit(0)
+
+
+
+
+
+
+conn.close()
